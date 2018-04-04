cd28d91e653e0320310f30acb35c380e220d1e27
fat19 <altin.fth19@gmail.com>
2018-04-05 00:21:03 +0300

    Weather tamamlandı

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMaps from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
@@ -9,10 +10,13 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const {lon, lat} = cityData.city.coord;
 
         return (
             <tr key={name}> 
-                <td>{name} </td>
+                <td>
+                    <GoogleMaps lat={lat} lon={lon} />
+                </td>
                 <td> 
                     <Chart data={temps} color="orange" units="K"/>
                 </td>
